34ec2375f9505b5a78679b9d2a14a42e9c774ea2
speedwarnsf <221411129+speedwarnsf@users.noreply.github.com>
2025-09-24 14:33:23 -0700

    Update firebaseConfig.js

--- wellth-app-revised/firebaseConfig.js
+++ wellth-app-revised/firebaseConfig.js
@@ -5,13 +5,12 @@ import 'firebase/firestore'; // Import Firestore service
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
-  measurementId: "YOUR_MEASUREMENT_ID"
+  apiKey: "AIzaSyA1xZGmRMEZ-i7f_wfMlJuI-LkADKoI4Jo",
+  authDomain: "wellth-af20c.firebaseapp.com",
+  projectId: "wellth-af20c",
+  storageBucket: "wellth-af20c.firebasestorage.app",
+  messagingSenderId: "171051029356",
+  appId: "1:171051029356:web:6d3ecc068a587f3c635949",
 };
 
 // Initialize Firebase
